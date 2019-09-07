52048c74244568fc0b99bca8735e672b4f2c18a5
Lee Hounshell <lee.hounshell@gmail.com>
2019-09-07 13:34:35 -0700

    SET_TIME works

--- jetson_nano/monitor.py
+++ jetson_nano/monitor.py
@@ -2,22 +2,33 @@
 
 import serial	# http://pyserial.sf.net
 import time
+from datetime import datetime
+import struct
 
 port = '/dev/ttyACM0' # note I'm using Jetson Nano
-ard = serial.Serial(port, 9600, timeout=5)
+arduino = serial.Serial(port, 9600, timeout=5)
 time.sleep(2) # wait for Arduino
 
-#   command = 'H' # Halt
-#   ard.flush()
-#   print("Python value sent: ")
-#   encoded_command = command.encode();
-#   ard.write(encoded_command)
-#   print(encoded_command)
+arduino.flush()
+command = 'X' # SET TIME
+encoded_command = command.encode();
+arduino.write(encoded_command)
+arduino.flush()
+time.sleep(1)
+
+now = datetime.now()
+timestamp = int(datetime.timestamp(now))
+commdata = arduino.write(struct.pack('>L', timestamp))
+arduino.flush()
+time.sleep(1)
+
+print("SET_TIME offset sent: ")
+print(str(timestamp))
 
 while True:
     time.sleep(1) # wait for Arduino
-    ard.flush()
-    msg = ard.read(ard.inWaiting()) # read all characters in buffer
+    arduino.flush()
+    msg = arduino.read(arduino.inWaiting()) # read all characters in buffer
     text = msg.decode().strip()
     if text != '':
         print(text)
