6eaa16ac563efbb353ec2f55cf3b4b2e31fb2fdf
Lee Hounshell <lee.hounshell@gmail.com>
2019-09-06 21:38:57 -0700

    unnecessary include and new monitor.py

--- jetson_nano/batbot.py
+++ jetson_nano/batbot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import serial
-import syslog
 import time
 
 #The following line is for serial over GPIO
---
+++ jetson_nano/monitor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import serial	# http://pyserial.sf.net
+import time
+
+port = '/dev/ttyACM0' # note I'm using Jetson Nano
+ard = serial.Serial(port, 9600, timeout=5)
+time.sleep(2) # wait for Arduino
+
+#   command = 'H' # Halt
+#   ard.flush()
+#   print("Python value sent: ")
+#   encoded_command = command.encode();
+#   ard.write(encoded_command)
+#   print(encoded_command)
+
+while True:
+    time.sleep(1) # wait for Arduino
+    ard.flush()
+    msg = ard.read(ard.inWaiting()) # read all characters in buffer
+    print(msg.decode())
