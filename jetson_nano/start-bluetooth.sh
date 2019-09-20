6d7258b0ed93c4610794350b6467cc1f6329e6e2
Lee Hounshell <lee.hounshell@gmail.com>
2019-09-20 11:17:33 -0700

    bugfix startup bluetooth script

--- jetson_nano/start-bluetooth.sh
+++ jetson_nano/start-bluetooth.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd /home/lee
-sleep 20
+sleep 30
 
 echo "bluetooth: initializing.."
 
 iw dev wlan0 set power_save off
 
 # needed for backward compatibility
-bluetootchctl add SP
+bluetoothctl add SP
 
 bluetoothctl select DC:71:96:1C:90:F7
 bluetoothctl power on
