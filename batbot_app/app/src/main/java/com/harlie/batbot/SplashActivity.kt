0323c447a57a148dbf705f3a757c55a5fe1d7a79
Lee Hounshell <lee.hounshell@gmail.com>
2019-09-20 15:47:45 -0700

    fix initial transition

--- batbot_app/app/src/main/java/com/harlie/batbot/SplashActivity.kt
+++ batbot_app/app/src/main/java/com/harlie/batbot/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 class SplashActivity : AppCompatActivity() {
@@ -12,9 +14,16 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate")
         super.onCreate(savedInstanceState)
-        val blueIntent: Intent = Intent(this, BluetoothActivity::class.java)
-        blueIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        startActivity(blueIntent)
-        finish()
+        Timer().schedule(1000) {
+            Log.d(TAG, "-Splash-")
+            val blueIntent: Intent = Intent(this@SplashActivity, BluetoothActivity::class.java)
+            blueIntent.setFlags(
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    or Intent.FLAG_ACTIVITY_NEW_TASK
+                    or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(blueIntent)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            finish()
+        }
     }
 }
