e3e15abc89d0ffbc0648a80cc4945937d93fe15a
Lee Hounshell <lee.hounshell@gmail.com>
2019-09-21 15:57:13 -0700

    reduce log lines to 8

--- batbot_app/app/src/main/java/com/harlie/batbot/util/LoggingTextTail.kt
+++ batbot_app/app/src/main/java/com/harlie/batbot/util/LoggingTextTail.kt
@@ -6,7 +6,7 @@ import android.util.Log
 public class LoggingTextTail {
     val TAG = "LEE: <" + LoggingTextTail::class.java.getName() + ">";
 
-    val MAX_LOG_LINES = 9
+    val MAX_LOG_LINES = 8
     val MAX_LOG_DISPLAY_CONTENT_CHARS = 300;
 
     var content = mutableListOf<String>()
