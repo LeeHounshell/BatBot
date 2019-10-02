167a831d9bce90a68056e133c084524f7d8b0177
Lee Hounshell <lee.hounshell@gmail.com>
2019-10-02 10:01:27 -0700

    autostart the identify.py server if not running

--- jetson_nano/capture_and_identify.sh
+++ jetson_nano/capture_and_identify.sh
@@ -13,10 +13,17 @@ then
     resolution="hd"
 fi
 
-./capture.sh ${image_name} ${resolution}
-
-python3 ./request_identify.py ${image_name} 2> /dev/null
-
 echo
-echo "Location: ${image_name}"
-
+if pgrep -x "identify.py" > /dev/null
+then
+    ./capture.sh ${image_name} ${resolution}
+    python3 ./request_identify.py ${image_name} 2> /dev/null
+else
+    echo "starting the identify server.."
+    echo "please wait 1 minute."
+    echo
+    echo "then retry."
+    echo "the first request will be slow."
+    echo "after that it speeds up."
+    nohup ./identify.py > /dev/null 2>&1 &
+fi
